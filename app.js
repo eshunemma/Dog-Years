886271370aadacd42bb3a0a3becf575ebd5a0dba
Emmanuel Eshun <91864345+eshunemma@users.noreply.github.com>
2021-10-10 23:45:49 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// my age 
+const myAge = 55; 
+// variable early years
+let earlyYears = 2; 
+earlyYears *= 10.5; 
+// 2 years from my age
+let laterYears = myAge - 2; 
+// multiply 4 and reassign it to laterYears 
+laterYears *= 4; 
+ // print earlyYears
+console.log(earlyYears);
+// print laterYears
+console.log(laterYears); 
+// adding earlyYears and LaterYears
+let myAgeInDogYears = earlyYears + laterYears; 
+// variable myName converts name to lowercase
+let myName = 'Emma'.toLowerCase();
+// print final statement
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
